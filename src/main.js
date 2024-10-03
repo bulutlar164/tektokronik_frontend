fa2de01e37904a14ce79d8792fbbcad15b4a737a
emirb <emir.bulut1@outlook.com>
2024-10-03 18:43:59 +0300

    Genel_Düzeltme

--- src/main.js
+++ src/main.js
@@ -11,11 +11,13 @@ import { createRouter,createWebHistory} from 'vue-router'
 import RouterTest from './views/RouterTest.vue';
 import RouterTest2 from './views/RouterTest2.vue';
 import Login from './views/Login.vue';
+// Şevket Admin
 
 const routes = [
     {path:"/",component:RouterTest},
     {path:"/Admin",component:RouterTest2},
     {path:"/Login",component:Login}
+    //Sevket Admin
 ]
 
 const router = createRouter({
